888cb3a34ab221fe02319cda0b39f194b0e21961
LearnCmakeGit <96665483+LearnCmakeGit@users.noreply.github.com>
2026-06-20 17:24:46 -0400

    Add Day01 Python starter script

---
+++ day01/main.py
@@ -0,0 +1,21 @@
+def square(x):
+    return x * x
+
+
+def main():
+    print('Hello from Rino-learning Day01!')
+
+    a = 3
+    b = 5
+
+    print('a + b =', a + b)
+
+    print('Numbers from 0 to 9:')
+    for i in range(10):
+        print(i)
+
+    print('square(5) =', square(5))
+
+
+if __name__ == '__main__':
+    main()
