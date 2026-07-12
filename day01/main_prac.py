c970fa12a4581928e8abc3057ccb630adb2c7474
LearnCmakeGit <96665483+LearnCmakeGit@users.noreply.github.com>
2026-07-12 17:53:52 -0400

    Update main_prac.py

--- day01/main_prac.py
+++ day01/main_prac.py
@@ -6,7 +6,8 @@ def main_prac():
     print("5 square", square(5))
     
     for i in range(10):
-        print(i)
+        if i >5 :
+            print(i)
 
 if __name__ == '__main__':
     main_prac()
