6261e64b8f550e0eabd0d844a375c56ee0db6c28
LearnCmakeGit <96665483+LearnCmakeGit@users.noreply.github.com>
2026-08-09 20:50:41 -0400

    Update numpy_matrix_prac.py

--- day02/numpy_matrix_prac.py
+++ day02/numpy_matrix_prac.py
@@ -5,7 +5,8 @@ import numpy as np
 def main():
     A = np.array([[1,2],[3,4]])
     b = np.array([1,2])
-    
+    print("A shape: ", A.shape)
+    print("b shape: ", b.shape)
     x = np.array([0,0])
     r = b-A@x
     print ("residual: ", np.linalg.norm(r))
