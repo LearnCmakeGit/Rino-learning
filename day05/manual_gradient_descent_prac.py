7b9746b2a592dc15c09cdde5f31f25e480808343
LearnCmakeGit <96665483+LearnCmakeGit@users.noreply.github.com>
2026-08-28 14:47:45 -0400

    Update manual_gradient_descent_prac.py

--- day05/manual_gradient_descent_prac.py
+++ day05/manual_gradient_descent_prac.py
@@ -15,8 +15,8 @@ def main():
         [7.0]
         ])
     
-    w = torch.tensor([[0.0]], requires_grad = True)
-    b = torch.tensor([[0.0]], requires_grad = True)
+    w = torch.tensor([0.0],requires_grad = True)
+    b = torch.tensor([0.0],requires_grad = True)
 
     learn_rate = 0.1
     num_steps = 100;
