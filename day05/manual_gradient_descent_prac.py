957c91620e5bcf576b54aa3dc4cfeca309662b79
LearnCmakeGit <96665483+LearnCmakeGit@users.noreply.github.com>
2026-08-28 14:19:30 -0400

    Create manual_gradient_descent_prac.py

---
+++ day05/manual_gradient_descent_prac.py
@@ -0,0 +1,40 @@
+
+import torch
+
+def main():
+    x=torch.tensor([
+        [0.0],
+        [1.0],
+        [2.0],
+        [3.0]
+        ])
+    y_true = torch.tensor([
+        [1.0],
+        [3.0],
+        [5.0],
+        [7.0]
+        ])
+    
+    w = torch.tensor([[0.0]], requires_grad = true)
+    b = torch.tensor([[0.0]], requires_grad = true)
+
+    learn_rate = 0.1
+    num_steps = 100;
+    for i in range(num_steps):
+        y = x@w.T + b
+        loss = torch.mean(y-y_true)
+        loss.backword()
+        
+        with torch.no_grad():
+            w += w.grad()
+            b += b.grad()
+     
+    print("w = ", w.item())
+    print("b = ", b.item())
+            
+    
+if __name__=="__main__":
+    main()
+
+
+
