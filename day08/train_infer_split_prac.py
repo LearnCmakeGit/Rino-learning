3a29335e822d877b9c6eba0b3f3b813c3efd7e7b
LearnCmakeGit <96665483+LearnCmakeGit@users.noreply.github.com>
2026-09-07 20:05:32 -0400

    Create train_infer_split_prac.py

---
+++ day08/train_infer_split_prac.py
@@ -0,0 +1,62 @@
+from pathlib import Path
+import torch
+from torch.utils.data import Dataset, DataLoader
+
+class LineDataset(Dataset):
+    def __init__(self):
+        self.x = torch.tensor([[0.0],[1.0],[2.0],[3.0]])
+        self.y_true = 2.0 * self.x + 1.0
+    
+    def __len__(self):
+        return len(self.x)
+    
+    def __getitem__(self, index):
+        return (self.x[index], self.y_true[index])
+    
+def model_train():
+    dataset = LineDataset()
+    dataloader = DataLoader(
+                 dataset,
+                 batch_size = 2,
+                 shuffle = True)
+    
+    model = nn.Linear(in_features = 1, out_features = 1)
+    
+    with torch.no_grad():
+        model.weight.fill(0.0)
+        model.bias.fill(0.0)
+    ls_fn = nn.MSELoss()
+    opt = torch.optim.SGD(model.parameters(), lr = 0.1)
+    
+    num_steps = 100
+    for step in range(num_steps):
+        epoch_loss = 0.0
+        for (batch_x, batch_yt) in dataloader:
+            y = model(batch_x)
+            ls = ls_fn(y,batch_yt)
+            opt.zero_grad()
+            ls.backward()
+            opt.step()
+        epoch_loss += ls.item()
+    print(" Train complete")
+    
+def model_infer(model):
+    x_test = torch.tensor([[10.0], [11.0]])
+    with torch.no_grad():
+        y_test = model(x_test)
+    print("x_test: ", x_test.tolist())
+    print("y_prod: ", y_test.tolist())
+    
+    
+                
+
+
+
+def main():
+    model = model_train()
+    model_infer(model)
+    
+    
+
+if __name__ == "__main__":
+    main()
