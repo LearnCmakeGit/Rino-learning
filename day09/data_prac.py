8c2e8b5256fe2d4c431b15a8a0f2aac4907e8b0f
LearnCmakeGit <96665483+LearnCmakeGit@users.noreply.github.com>
2026-09-11 23:03:01 -0400

    Create data_prac.py

---
+++ day09/data_prac.py
@@ -0,0 +1,13 @@
+import torch
+from torch.nn.data import Dataset
+
+def LineData():
+    
+    def __init__(self):
+        
+        self.x = torch.tensor([[0.0],[1.0],[2.0],[3.0]])
+        slef.y_true=torch.tensor([[1.0],[3.0],[5.0],[7.0]])
+
+    def len(self):
+        return 
+    
