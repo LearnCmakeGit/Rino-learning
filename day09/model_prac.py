727aad568395994b09a91dbab0273a63eddebc0b
LearnCmakeGit <96665483+LearnCmakeGit@users.noreply.github.com>
2026-09-12 19:54:00 -0400

    Update model_prac.py

--- day09/model_prac.py
+++ day09/model_prac.py
@@ -3,7 +3,7 @@ from torch import nn
 
 def model_prac(nn.Modules):
     def __init__(self):
-        super():__init__()
+        super().__init__()
         self.lin = nn.Linear(in_features = 1, out_features = 1)
     
     def forward(self):
