a9b61b377743c482f32c2b35ed39add598719529
LearnCmakeGit <96665483+LearnCmakeGit@users.noreply.github.com>
2026-09-12 20:01:57 -0400

    Update data_prac.py

--- day09/data_prac.py
+++ day09/data_prac.py
@@ -6,7 +6,7 @@ class LineData():
     def __init__(self):
         
         self.x = torch.tensor([[0.0],[1.0],[2.0],[3.0]])
-        slef.y_true=torch.tensor([[1.0],[3.0],[5.0],[7.0]])
+        self.y_true=torch.tensor([[1.0],[3.0],[5.0],[7.0]])
 
     def __len__(self):
         return len(self.x)
