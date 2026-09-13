1da74ccf80b6a301e48d7e90d1670c8141762cc2
LearnCmakeGit <96665483+LearnCmakeGit@users.noreply.github.com>
2026-09-12 21:35:53 -0400

    Update infer_prac.py

--- day09/infer_prac.py
+++ day09/infer_prac.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 import torch
 from torch import nn
 
@@ -5,6 +6,9 @@ from model_prac import model_prac
 
 def main():
     model = model_prac()
+    checkpoint = Path(__file__).resolve().parent/"checkpoint.pt"
+    state = torch.load(checkpoint)
+    model.load_state_dict(state)
     
     state = torch.
     
