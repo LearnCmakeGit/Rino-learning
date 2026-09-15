9733f655a7e1b82254d086238dd8caf437fc8834
LearnCmakeGit <96665483+LearnCmakeGit@users.noreply.github.com>
2026-09-15 10:42:43 -0400

    Create train_prac.py

---
+++ day10/train_prac.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+import torch
+from torch import nn
+
+def main():
+    x = torch.Linspace(-2,2,41).reshape(-1,1)
+    y = x**2
+    
+    model = MLP()
+    ls_fn = nn.MSELoss()
+    opt = torch.optim(model.parameters(),lr=0.1)
+    model.train()
+    
+    num_steps =1000
+    for step in range(num_steps):
+        y_pred = model(x)
+        ls = ls_fn(y,y_pred)
+        opt.zero_grad()
+        ls.backward()
+        opt.step()
+        if step %100 == 0 or step == num_steps-1:
+            print(f"step {step:3d}, loss: {ls.item():.4e}")
+    checkpoint = Path(__file__).resolve().parent/"mlp.pt"
+    torch.save(model.state_dict(),checkpoint)
+    
+        
+        
+
+
+
+
+if __name__ == "__main__":
+    main()
