93e7bac9e3fef3e01d2599f2ccff5d13c9f91d44
LearnCmakeGit <96665483+LearnCmakeGit@users.noreply.github.com>
2026-09-15 11:21:34 -0400

    Update model_prac.py

--- day10/model_prac.py
+++ day10/model_prac.py
@@ -6,7 +6,7 @@ class MLP(nn.Module):
     def __init__(self, in_features, hidden_features, out_features):
         super().__init__()
         
-        slef.net = nn.sequential(
+        slef.net = nn.Sequential(
             nn.linear(in_features, hidden_features),
             nn.Relu(),
             nn.linear(hidden_features, out_features)
