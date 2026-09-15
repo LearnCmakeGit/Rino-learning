9bc500e599cc8b1ccec6e4537b51bc9a371ace0c
LearnCmakeGit <96665483+LearnCmakeGit@users.noreply.github.com>
2026-09-15 10:55:51 -0400

    Create model_prac.py

---
+++ day10/model_prac.py
@@ -0,0 +1,15 @@
+import torch
+from torch import nn
+
+class MLP(nn.Module):
+    super():__init__()
+    
+    def __init__(self):
+        slef.net = nn.sequential{
+            nn.linear(in_features=1, 
+            nn.Relu()
+            nn.linear()
+        }
+        
+    def forward(self,x):
+        return self.net(x)
