3b8358dedf68dad1510497da30def37dbfcd64fe
LearnCmakeGit <96665483+LearnCmakeGit@users.noreply.github.com>
2026-09-19 14:10:56 -0400

    Create infer_prac.py

---
+++ day10/infer_prac.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+import torch
+from model_prac import MLP
+
+
+def main():
+    model = MLP(in_features=1, hidden_features=8, out_features=1)
+    
+    checkpoint= Path(__file__).resolve().parent/"MLP.pt"
+    state = torch.load(checkpoint, location="cpu")
+    model.load_state_dict(state)
+    
+    test = torch.tensor([[10.0],[15.0]])
+    with torch.no_grad():
+        y_pred = model(test)
+    
+    print("pred y: ", y_pred.tolist())
+    
+
+
+if __name__ == "__main__":
+    main()
